0c9a860841536e1d8ab9a84ad5fe2d7afcd42a05
Bonnielamb6 <edgartrabajos2205@gmail.com>
2023-09-01 11:50:09 -0600

    Prueba

---
+++ src/back/Productores.java
@@ -0,0 +1,14 @@
+package back;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+public class Productores {
+    a
+}
